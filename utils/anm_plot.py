abc2666782aecdb0723ecc021ea04e4157c13d4a
Michał Januszewski <spock@gentoo.org>
2010-04-29 20:43:11 +0200

    Make it possible to use anm_plot interactively.

--- utils/anm_plot.py
+++ utils/anm_plot.py
@@ -22,99 +22,103 @@ import matplotlib.pyplot as plt
 
 from pylab import *
 
-fname = sys.argv[1]
-xvar = sys.argv[2]
-yvar = sys.argv[3]
+def make_plot(dplot, slicearg, data, pars, xvar, yvar, xidx, yidx):
+    a = abs(min(np.min(dplot[slicearg]), 0.0))
+    b = abs(np.max(dplot[slicearg]))
+    sc = a / (a+b) / 0.5
+    sc2 = b / (a+b) / 0.5
 
-data = np.load(fname)
-pars = list(data['par_multi']) + list(data['scan_vars'])
+    anm = {
+        'red': [
+            (0., 0.0, 0.0),
+            (sc*0.5, 1.0, 1.0),
+            (sc*0.5 + (0.817460-0.5)*sc2, 1.0, 1.0),
+            (1.0, 0.8, 0.8)],
 
-print 'Parameters are (in order): ', pars
+        'green': [
+            (0., 0.0, 0.0),
+            (sc*0.4, 1.0, 1.0),
+            (sc*0.5, 1.0, 1.0),
+            (sc*0.5 + (0.626984-0.5)*sc2, 1.0, 1.0),
+            (sc*0.5 + (0.817460-0.5)*sc2, 0.6, 0.6),
+            (1.0, 0.0, 0.0)],
 
-idxs = set(range(len(pars)))
-xidx = pars.index(xvar)
-yidx = pars.index(yvar)
-idxs.remove(xidx)
-idxs.remove(yidx)
+        'blue': [
+            (0.0, 0.4, 0.4),
+            (sc*0.25, 1.0, 1.0),
+            (sc*0.5, 1.0, 1.0),
+            (sc*0.5 + (0.626984-0.5)*sc2, 0., 0.),
+            (1.0, 0.0, 0.0)]
+        }
 
-# Now scan the command line arguments for info about
-# which values to use for the remanining parameters
-argidx = 4
-slicearg = []
+    if sc == 0.0:
+        for k in anm.iterkeys():
+            anm[k] = anm[k][1:]
 
-for i in range(len(pars)):
-    if i in idxs:
-        slicearg.append(int(sys.argv[argidx]))
+    anm_cmap = LinearSegmentedColormap('ANM', anm)
+    plt.register_cmap(cmap=anm_cmap)
+
+    aspect = ((data[pars[xidx]][-1] - data[pars[xidx]][0]) /
+              (data[pars[yidx]][-1] - data[pars[yidx]][0])) / 1.5
+
+    imshow(dplot[slicearg],
+           extent=(data[pars[xidx]][0], data[pars[xidx]][-1],
+                   data[pars[yidx]][0], data[pars[yidx]][-1]),
+           aspect=aspect,
+           interpolation='bilinear',
+#       interpolation='nearest',
+           cmap = anm_cmap,
+           origin='lower')
+    ylabel(yvar)
+    xlabel(xvar)
+    colorbar()
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    xvar = sys.argv[2]
+    yvar = sys.argv[3]
+
+    data = np.load(fname)
+    pars = list(data['par_multi']) + list(data['scan_vars'])
+
+    print 'Parameters are (in order): ', pars
+
+    idxs = set(range(len(pars)))
+    xidx = pars.index(xvar)
+    yidx = pars.index(yvar)
+    idxs.remove(xidx)
+    idxs.remove(yidx)
+
+    # Now scan the command line arguments for info about
+    # which values to use for the remanining parameters
+    argidx = 4
+    slicearg = []
+
+    for i in range(len(pars)):
+        if i in idxs:
+            slicearg.append(int(sys.argv[argidx]))
+            argidx += 1
+            print '%s = %s' % (pars[i], data[pars[i]][slicearg[-1]])
+        else:
+            slicearg.append(slice(None))
+
+    if data['main'].shape[-1] > 1:
+        slicearg.append(sys.argv[argidx])
         argidx += 1
-        print '%s = %s' % (pars[i], data[pars[i]][slicearg[-1]])
     else:
-        slicearg.append(slice(None))
+        slicearg.append(0)
 
-if data['main'].shape[-1] > 1:
-    slicearg.append(sys.argv[argidx])
-    argidx += 1
-else:
-    slicearg.append(0)
+    dplot = data['main']
 
-dplot = data['main']
+    if xidx < yidx:
+        dplot = np.swapaxes(dplot, xidx, yidx)
 
-if xidx < yidx:
-    dplot = np.swapaxes(dplot, xidx, yidx)
+    print 'min/max: ', np.min(dplot[slicearg]), np.max(dplot[slicearg])
 
-print 'min/max: ', np.min(dplot[slicearg]), np.max(dplot[slicearg])
+    make_plot(dplot, slicearg, data, pars, xvar, yvar, xidx, yidx)
 
-a = abs(min(np.min(dplot[slicearg]), 0.0))
-b = abs(np.max(dplot[slicearg]))
-sc = a / (a+b) / 0.5
-sc2 = b / (a+b) / 0.5
-
-anm = {
-    'red': [
-        (0., 0.0, 0.0),
-        (sc*0.5, 1.0, 1.0),
-        (sc*0.5 + (0.817460-0.5)*sc2, 1.0, 1.0),
-        (1.0, 0.8, 0.8)],
-
-    'green': [
-        (0., 0.0, 0.0),
-        (sc*0.4, 1.0, 1.0),
-        (sc*0.5, 1.0, 1.0),
-        (sc*0.5 + (0.626984-0.5)*sc2, 1.0, 1.0),
-        (sc*0.5 + (0.817460-0.5)*sc2, 0.6, 0.6),
-        (1.0, 0.0, 0.0)],
-
-    'blue': [
-        (0.0, 0.4, 0.4),
-        (sc*0.25, 1.0, 1.0),
-        (sc*0.5, 1.0, 1.0),
-        (sc*0.5 + (0.626984-0.5)*sc2, 0., 0.),
-        (1.0, 0.0, 0.0)]
-    }
-
-if sc == 0.0:
-    for k in anm.iterkeys():
-        anm[k] = anm[k][1:]
-
-anm_cmap = LinearSegmentedColormap('ANM', anm)
-plt.register_cmap(cmap=anm_cmap)
-
-aspect = ((data[pars[xidx]][-1] - data[pars[xidx]][0]) /
-          (data[pars[yidx]][-1] - data[pars[yidx]][0])) / 1.5
-
-imshow(dplot[slicearg],
-       extent=(data[pars[xidx]][0], data[pars[xidx]][-1],
-               data[pars[yidx]][0], data[pars[yidx]][-1]),
-       aspect=aspect,
-#       interpolation='sinc',
-       interpolation='nearest',
-       cmap = anm_cmap,
-       origin='lower')
-ylabel(yvar)
-xlabel(xvar)
-colorbar()
-
-if len(sys.argv) > argidx:
-    savefig(sys.argv[argidx])
-else:
-    ion()
-    show()
+    if len(sys.argv) > argidx:
+        savefig(sys.argv[argidx])
+    else:
+        ion()
+        show()
