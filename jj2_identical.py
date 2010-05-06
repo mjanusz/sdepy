1aa639230d516fec35dc923697a35228060be13f
Michał Januszewski <spock@gentoo.org>
2010-05-06 22:58:32 +0200

    Fix the system of two identical JJs.

--- jj2_identical.py
+++ jj2_identical.py
@@ -16,25 +16,24 @@ def init_vector(sdei, i):
 
 sim_params = {'i1': 'constant force on the 1st particle',
               'i2': 'constant force on the 2nd particle',
-              'ic1': 'critical current',
               'alpha': 'coupling strength',
               'd0': 'noise strength',
-              'a1': 'AC drive amplitude',
+              'a1': 'AC drive amplitude for the 1st particle',
+              'a2': 'AC drive amplitude for the 2nd particle',
               'omega': 'AC drive frequency'}
 
 local_vars = { 
-        'ns0': lambda sdei: sympy.sqrt(sdei.S.d0 * sdei.S.dt * 2.0),
-        'ns1': lambda sdei: sympy.sqrt(sdei.S.d0 * sdei.S.dt * 2.0),
+        'ns0': lambda sdei: sympy.sqrt(sdei.S.d0 * sdei.S.dt * (1.0 / sdei.S.alpha - sdei.S.alpha)),
         }
 
 code = """
-    dx0 = i1 - ic1 * sinf(x0) - alpha * (i2 - ic1 * sinf(x1)) + a1 * sinf(omega * t);
-    dx1 = i2 - ic1 * sinf(x1) - alpha * (i1 - ic1 * sinf(x0) + a1 * sinf(omega * t));
+    dx0 = i1 - sinf(x0) + alpha * (i2 - sinf(x1)) + (a1 + alpha * a2) * sinf(omega * t);
+    dx1 = i2 - sinf(x1) + alpha * (i1 - sinf(x0)) + (alpha * a1 + a2) * sinf(omega * t);
 """
 
-ns_map = {0: [0, 0], 1: [0, 0]}
-period_map = {0: sde.PeriodInfo(period=2.0 * math.pi, freq=1.0),
-              1: sde.PeriodInfo(period=2.0 * math.pi, freq=1.0)}
+ns_map = {0: ['ns0', 0], 1: [0, 'ns0']}
+period_map = {0: sde.PeriodInfo(period=2.0 * math.pi, freq=1),
+              1: sde.PeriodInfo(period=2.0 * math.pi, freq=1)}
 
 sdei = sde.SDE(code, sim_params, num_vars=2, num_noises=2, noise_map=ns_map, period_map=period_map,
                local_vars=local_vars)
