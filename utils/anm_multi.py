ca5929f91e62844cb86ecee65821e80a20c32059
Michał Januszewski <spock@gentoo.org>
2010-06-16 14:40:14 +0200

    Add support for 2x1 multi plots in anm_multi.

--- utils/anm_multi.py
+++ utils/anm_multi.py
@@ -8,10 +8,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid import AxesGrid
 from pylab import *
 
-nr = 2
-nc = 2
 
 options, args = anm_plot.parse_opts()
+
+nc = 2
+nr = options.panels / nc
 
 # Spacing and size of the colorbar as fractions of the figure size.
 space = 0.01
@@ -21,35 +22,48 @@ effective_w = 1.0 - cbar_fraction - space
 # The panel aspect if the controlling variable and the total figure apsect is
 # automatically adjusted.
 options.figaspect = effective_w * (1.0/options.panel_aspect)
+if nr == 1:
+    options.figaspect /= 2.0
+
 anm_plot.set_opts()
 
 fig = figure()
 cax = fig.add_axes([1.0 - cbar_fraction, 0.0, cbar_fraction, 1.0])
 
 w = effective_w/2.0 - space/2.0
-h = 0.5 - space/2.0
+h = 1.0/nr - space/2.0
+
+if nr == 1:
+    h = 1.0
 
 axs = []
 anm_args = []
 labels = ['$(a)$', '$(b)$', '$(c)$', '$(d)$']
 i = 0
 
-for y in reversed(range(nc)):
-    for x in range(nr):
-        pos = [x*(effective_w/2.0 + space/2.0), y*(0.5 + space/2.0), w, h]
+for y in reversed(range(nr)):
+    for x in range(nc):
+        pos = [x*(effective_w/2.0 + space/2.0), y*(1.0/nr + space/2.0), w, h]
         a = fig.add_axes(pos)
         if y > 0:
             a.set_xticklabels([])
         if x > 0:
             a.set_yticklabels([])
 
-        t = fig.text(pos[0] + 0.05, pos[1] + h - 0.075, labels[i], horizontalalignment='left') #fontproperties=FontProperties(size=16))
+        if nr == 1:
+            corr = 0.075
+        else:
+            corr = 0
+
+        t = fig.text(pos[0] + 0.05, pos[1] + h - 0.075 - corr, labels[i], horizontalalignment='left') #fontproperties=FontProperties(size=16))
         a.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(prune='both'))
         a.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(prune='both'))
         axs.append(a)
         i += 1
 a = -999999
 b = -999999
+
+xvars = []
 
 for ax in axs:
     fname = args[0]
@@ -59,6 +73,7 @@ for ax in axs:
 
     xvar = args[1]
     yvar = args[2]
+    xvars.append(xvar)
 
     idxs = set(range(len(pars)))
     xidx = pars.index(xvar)
@@ -82,14 +97,13 @@ for ax in axs:
     args = args[i:]
 
 i = 0
-for y in reversed(range(nc)):
-    for x in range(nr):
+for y in reversed(range(nr)):
+    for x in range(nc):
         ax = axs[i]
         if x == 0:
             ax.set_ylabel(anm_plot.var_display(yvar))
         if y == 0:
-            ax.set_xlabel(anm_plot.var_display(xvar))
-
+            ax.set_xlabel(anm_plot.var_display(xvars[i]))
 
         i += 1
 
