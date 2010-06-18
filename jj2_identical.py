a129618b7f9214b339f240805b57e8d57cf226bc
Michał Januszewski <spock@gentoo.org>
2010-06-18 22:11:17 +0200

    Add support for random initial phase of the AC driving in the JJ2 simulation.

--- jj2_identical.py
+++ jj2_identical.py
@@ -11,6 +11,11 @@ import sys
 def init_vector(sdei, i):
     return numpy.random.uniform(0.0, 2.0 * numpy.pi, sdei.num_threads)
 
+# Random initial phase of the AC drive.
+const_pars = {
+        'phi0': lambda sdei: numpy.random.uniform(0.0, 2.0 * numpy.pi, sdei.num_threads)
+    }
+
 sim_params = {'i1': 'constant force on the 1st particle',
               'i2': 'constant force on the 2nd particle',
               'alpha': 'coupling strength',
@@ -24,15 +29,15 @@ local_vars = {
         }
 
 code = """
-    dx0 = i1 - sinf(x0) + alpha * (i2 - sinf(x1)) + (a1 + alpha * a2) * cosf(omega * t);
-    dx1 = i2 - sinf(x1) + alpha * (i1 - sinf(x0)) + (alpha * a1 + a2) * cosf(omega * t);
+    dx0 = i1 - sinf(x0) + alpha * (i2 - sinf(x1)) + (a1 + alpha * a2) * cosf(omega * t + phi0);
+    dx1 = i2 - sinf(x1) + alpha * (i1 - sinf(x0)) + (alpha * a1 + a2) * cosf(omega * t + phi0);
 """
 
 ns_map = {0: ['ns0', 0], 1: [0, 'ns0']}
 period_map = {0: sde.PeriodInfo(period=2.0 * math.pi, freq=1),
               1: sde.PeriodInfo(period=2.0 * math.pi, freq=1)}
 
-sdei = sde.SDE(code, sim_params, num_vars=2, num_noises=2, noise_map=ns_map, period_map=period_map,
+sdei = sde.SDE(code, sim_params, num_vars=2, num_noises=2, const_pars=const_pars, noise_map=ns_map, period_map=period_map,
                local_vars=local_vars)
 
 output = {'path': {
