ad4e4787b40bad9ec22a36ad913c965198230ec8
Michał Januszewski <spock@gentoo.org>
2011-01-09 10:36:45 +0100

    Add initial random phase of the AC drive to jj1.py.

--- jj1.py
+++ jj1.py
@@ -7,13 +7,17 @@ import math
 import numpy
 import sde
 import sympy
-import sys
 
 def init_vector(sdei, i):
     if i == 0:
         return numpy.random.uniform(0.0, 2.0 * math.pi, sdei.num_threads)
     else:
         return numpy.random.uniform(-2.0, 2.0, sdei.num_threads)
+
+# Random initial phase of the AC drive.
+const_pars = {
+        'phi0': lambda sdei: numpy.random.uniform(0.0, 2.0 * numpy.pi, sdei.num_threads)
+    }
 
 sim_params = {'force': 'DC bias current',
               'gam': 'damping constant',
@@ -25,14 +29,14 @@ local_vars = { 'ns': lambda sdei: sympy.sqrt(sdei.S.d0 * sdei.S.dt * 2.0 * sdei.
 
 code = """
     dx0 = x1;
-    dx1 = -sinf(x0) + amp * cosf(omega * t) + force - gam * x1;
+    dx1 = -sinf(x0) + amp * cosf(omega * t + phi0) + force - gam * x1;
 """
 
 ns_map = {1: ['ns']}
 period_map = {0: sde.PeriodInfo(period=2.0 * math.pi, freq=1)}
 
 sdei = sde.SDE(code, sim_params, num_vars=2, num_noises=1, noise_map=ns_map, period_map=period_map,
-               local_vars=local_vars)
+               local_vars=local_vars, const_pars=const_pars)
 
 output = {'path': {
             'main': [sde.OutputDecl(func=sde.avg_moments, vars=[0])],
